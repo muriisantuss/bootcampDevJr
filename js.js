e09e9f7e5a5c6a8537a4f498d9c70a122ceca682
muriisantuss <muriisantuss@gmail.com>
2024-04-01 02:30:30 -0300

    closes #16 e #17 - Task 05: Initial History Logic & Task 06: Finalizing the History

--- js.js
+++ js.js
@@ -7,17 +7,29 @@ function calculate(operation) {
   const number1 = parseFloat(calculator.number1.value);
   const number2 = parseFloat(calculator.number2.value);
 
-  if (operation == 1){
+  if (operation == "+") {
     result = number1 + number2;
-  } 
-  if (operation == 2){
+  }
+  if (operation == "-") {
     result = number1 - number2;
-  } 
-  if (operation == 3){
+  }
+  if (operation == "*") {
     result = number1 * number2;
-  } 
-  if (operation == 4){
+  }
+  if (operation == "/") {
     result = number1 / number2;
-  } 
+  }
   document.getElementById("result").setAttribute("value", result);
+  history(number1, number2, operation, result);
+}
+
+function history(number1, number2, operation, result) {
+  const newListItem = document.createElement("li");
+  const historyElement = document.getElementById("history-list");
+
+  newListItem.textContent = `${number1} ${operation} ${number2} = ${result} `;
+  historyElement.prepend(newListItem);
+  if (historyElement.children.length > 10) {
+    historyElement.lastElementChild.remove();
+  }
 }
