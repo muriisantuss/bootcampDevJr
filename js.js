ef1d35c144575d864796e76eda553698686e17dc
muriisantuss <muriisantuss@gmail.com>
2024-04-01 00:04:08 -0300

    closes #15 - Task 04: Implement the 4 mathematical operations

--- js.js
+++ js.js
@@ -3,10 +3,21 @@ const calculator = {
   number2: document.getElementById("input2"),
 };
 
-function sum() {
+function calculate(operation) {
   const number1 = parseFloat(calculator.number1.value);
   const number2 = parseFloat(calculator.number2.value);
 
-  result = number1 + number2;
-  document.getElementById("result").setAttribute('value', result)
+  if (operation == 1){
+    result = number1 + number2;
+  } 
+  if (operation == 2){
+    result = number1 - number2;
+  } 
+  if (operation == 3){
+    result = number1 * number2;
+  } 
+  if (operation == 4){
+    result = number1 / number2;
+  } 
+  document.getElementById("result").setAttribute("value", result);
 }
